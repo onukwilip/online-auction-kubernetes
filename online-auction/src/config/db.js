6cf7c43b174c4f2ec8e9201df301074d64d31abd
onukwilip <onukwilip@gmail.com>
2025-05-23 19:25:13 +0100

    Modified app to db connection

--- online-auction/src/config/db.js
+++ online-auction/src/config/db.js
@@ -1,16 +1,28 @@
 const mongoose = require("mongoose");
 
+let retries = 10;
+
 const connect = async () => {
-  const con = await mongoose
-    .connect(process.env.MONGODB_URI)
-    .catch(async (e) => {
-      console.log(e);
-      await connect();
-    });
-  if (con) {
-    console.log(`Connected successfully`);
+  while (retries) {
+    try {
+      console.log("URI: ", process.env.MONGODB_URI);
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log(`✅ Connected successfully`);
+      break;
+    } catch (e) {
+      console.log(
+        `❌ MongoDB connection failed. Retrying in 10s...`,
+        e.message
+      );
+      retries--;
+      await new Promise((res) => setTimeout(res, 5000));
+    }
   }
-  // console.log("URI", process.env.MONGODB_URI);
+
+  if (!retries) {
+    console.error("🔥 Could not connect to MongoDB after multiple attempts.");
+    process.exit(1);
+  }
 };
 
 module.exports = connect;
