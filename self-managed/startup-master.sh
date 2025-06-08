df58652b1e4785e81cae5e36e6904ec318fc9a01
onukwilip <onukwilip@gmail.com>
2025-06-08 12:58:35 +0100

    Added the helm chart for monitoring; Added the scripts to setup a self-managed cluster

---
+++ self-managed/startup-master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update system and install Git
+sudo apt-get update -y
+sudo apt-get install -y git
+
+
+# Clone the repo
+REPO_URL="https://github.com/onukwilip/online-auction-kubernetes.git"
+CLONE_DIR="online_auction_k8s"
+
+git clone $REPO_URL
+
+cd $CLONE_DIR
+
+chmod +x ./common.sh
+chmod +x ./master.sh
+
+./common.sh
+
+./master.sh
+
