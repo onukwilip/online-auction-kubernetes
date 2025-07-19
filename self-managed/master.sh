ae95d286ca737e030dcc3a16fe18147a94a175cb
onukwilip <onukwilip@gmail.com>
2025-07-19 10:09:15 +0100

    Fix Calico Network Plugin installation commands in master.sh

--- self-managed/master.sh
+++ self-managed/master.sh
@@ -42,5 +42,6 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 # Install Claico Network Plugin Network
 
 # kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/custom-resources.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/operator-crds.yaml || true
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/tigera-operator.yaml ||true
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/custom-resources.yaml || true
